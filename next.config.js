eeca2a36759d4ec2623c53e3288c80d419faf748
Christian Juth <cjuth2@gmail.com>
2023-01-17 13:49:08 -0500

    disable minification

--- next.config.js
+++ next.config.js
@@ -3,6 +3,24 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  // swcMinify: true,
+  // transpilePackages: [
+  //   "@fullcalendar/core",
+  //   "@fullcalendar/daygrid",
+  //   "@fullcalendar/interaction",
+  //   "@fullcalendar/premium-common",
+  //   "@fullcalendar/react",
+  //   "@fullcalendar/resource-timeline",
+  //   "@fullcalendar/scrollgrid",
+  //   "@fullcalendar/timeline",
+  //   "@fullcalendar/resource",
+  //   "@fullcalendar/scrollgrid",
+  //   "@fullcalendar/timegrid", 
+  // ],
+  webpack: config => {
+    config.optimization.minimize = false
+    return config
+  }
 }
 
 module.exports = nextConfig
